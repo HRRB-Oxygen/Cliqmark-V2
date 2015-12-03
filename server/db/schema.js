de9a484fa40c55b4a594c39748aaf4a44595a42d
David Lee <david7lee@gmail.com>
2015-12-03 14:01:52 -0500

    created findRecs to query the database and return a single bookmark

--- server/db/schema.js
+++ server/db/schema.js
@@ -178,15 +178,27 @@ exports.findTags = function() {
 
 };
 
-//find recommendations for recently bookmarked site based on tagId
-exports.findRecs = function(tagId, bookmarkId, callback) {
-  Tag.findOne({ where: {tagName: tagName} })
-  var query = "SELECT tagId FROM BookmarkTags WHERE bookmarkId = " + bookmarkId + " AND tagId = " + tagId + ";";
-
-  sequelize.query(query).spread(function(results) {
-    callback();
-  });
-
+// Find recommendations for recently bookmarked site based on tagId
+// Should re-factor this into a single mysql query
+exports.findRecs = function(url, callback) {
+  // Find bookmark id for the url we're passing in
+  Bookmark.findOne({ where: { url: url }})
+  .then(function (bookmark) {
+    // Find the tagId of the current bookmark
+    BookmarkTags.findOne({ where: { bookmarkId: bookmark.id }} )
+    .then(function (bookmarkTag) {
+      // Once we have the tagId, find a different bookmark with the same tag
+      BookmarkTags.findOne({ where: { tagId: bookmarkTag.tagId }} )
+      .then(function (newBookmarkTag) {
+        // Now we have the id for our recommendation, so go back to bookmarks table and query it
+        Bookmark.findOne({ where: { id: newBookmarkTag.bookmarkId }} )
+        .then(function (recommendation) {
+          // Return the recommendation as an argument of a callback function
+          callback(recommendation);
+        })
+      })
+    })
+  })
 };
 
 //finds or creates a tag, and adds a join to the bookmark ID
